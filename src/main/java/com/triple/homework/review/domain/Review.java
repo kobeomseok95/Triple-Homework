98ba22c8a2c9c6ac0275ccba2bde82ee07651efa
kobeomseok95 <kobumssh@naver.com>
2022-06-23 21:44:44 +0900

    review 엔티티 설정

---
+++ src/main/java/com/triple/homework/review/domain/Review.java
@@ -0,0 +1,40 @@
+package com.triple.homework.review.domain;
+
+import com.triple.homework.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"USER_ID", "PLACE_ID"})
+})
+@SQLDelete(sql = "UPDATE REVIEW SET IS_DELETED = true WHERE REVIEW_ID = ?")
+@Where(clause = "DELETED=false")
+public class Review extends BaseEntity {
+
+    @Id
+    @Column(name = "REVIEW_ID", length = 36)
+    private String id;
+
+    @Column(length = 36, nullable = false)
+    private String userId;
+
+    @Column(length = 36, nullable = false)
+    private String placeId;
+
+    @Column(length = 1000)
+    private String content;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private boolean deleted;
+}
