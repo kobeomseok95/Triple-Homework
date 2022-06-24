3fcb7cf4bfe1ff0cdf6926b27511f9aecf3c09d9
kobeomseok95 <kobumssh@naver.com>
2022-06-24 15:08:14 +0900

    ApplicationException 클래스 작성

---
+++ src/main/java/com/triple/homework/common/exception/ApplicationException.java
@@ -0,0 +1,19 @@
+package com.triple.homework.common.exception;
+
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException() {
+    }
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApplicationException(Throwable cause) {
+        super(cause);
+    }
+}
