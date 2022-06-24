08899bc778d733b056cabbb0dbbe9545638d1c09
kobeomseok95 <kobumssh@naver.com>
2022-06-24 15:22:32 +0900

    ControllerAdvice 클래스 작성

---
+++ src/main/java/com/triple/homework/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.triple.homework.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ErrorResponse> applicationException(ApplicationException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(e));
+    }
+}
